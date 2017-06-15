b08bc501d0a7e82d7ee3e1a4dd554849c666a5f1
soyeon1128 <ksy4334334@gmail.com>
2017-06-15 19:21:04 +0900

    placeholder 추가, 입력창 미입력 시 경고창 추가

--- JavaScript/to_do_list/to_do_list.js
+++ JavaScript/to_do_list/to_do_list.js
@@ -14,14 +14,19 @@
     var ul = _.selector('.list-to-do');
 
     addition.onclick = function() {
-        
+
+        var data = input_box.value;        
+        if( data === "" ) {
+            alert("입력값을 입력하세요.")
+            throw ("입력값을 입력하세요.")
+        };
+
         var li = _.createElement('li');
         var label = _.createElement('label');
 
         var checkbox = _.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
 
-        var data = input_box.value;
         var text = _.createText(data);
      
         _.appendChild(label, checkbox);
